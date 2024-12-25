bc7c4efdd1d50b955e13c4550154065dcc723368
umarudoma <schreiber@screenpaper.de>
2024-12-25 21:24:45 +0100

    geändert:       App.jsx 	neue Datei:     grid.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import reactImg from "./assets/react-core-concepts.png";
 import Timeblock from "./components/Timeblock";
 import Anrede from "./components/Anrede";
+import  "./grid.css";
 import Morgensatz from "./components/Morgensatz";
 function App() {
   return (
-    <div>
-      <header>
-        <img src={reactImg} alt="Stylized atom" />
-        <h1>Hallo <Anrede /></h1>
-        <p>
-           <Morgensatz />
-        </p>
-      </header>
-      <main>
-        <h1> <Timeblock /> </h1>
-      </main>
-    </div>
+ //   <div>
+ //     <header>
+ //        <img src={reactImg} alt="Stylized atom" />
+ //        <h1>Hallo <Anrede /></h1>
+ //        <p>
+ //           <Morgensatz />
+  //       </p>
+  //     </header>
+  //     <main>
+  //       <h1> Heute : <Timeblock /> </h1>
+  //     </main>
+ //   </div>
+     <div id="grid">
+	  <div id="title">Hallo <Anrede /> ..... <Morgensatz /> <p> Heute : <Timeblock /> </p></div>
+	  <div id="score">Wir kommen gleich vorbei</div>
+	  <div id="stats"><p>Heute</p><p>9:00 Frühstück</p><p>12:00 Es ist jetzt Zeit für  Mittagessen</p><p>18:00 Abendessen</p><p>Morgen</p><p>9:00 Frühstück</p><p>12:00 Mittagessen</p><p>15:00 Friseur</p><p>18:00 Abendessen</p></div>
+	  <div id="board"><img src={reactImg} alt="Stylized atom" /></div>
+	  <div id="controls"><p align="left">Hallo</p><p align="right">Hallo hier</p></div>
+	</div>
   );
 }
 
