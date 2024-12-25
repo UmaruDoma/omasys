cbf0c741f9c6831e0f98b38f7078d2ba1dbead98
umarudoma <schreiber@screenpaper.de>
2024-12-25 22:04:42 +0100

    geändert:       App.jsx 	geändert:       components/Timeblock.jsx 	geändert:       index.css

--- src/App.jsx
+++ src/App.jsx
@@ -18,11 +18,11 @@ function App() {
   //     </main>
  //   </div>
      <div id="grid">
-	  <div id="title">Hallo <Anrede /> ..... <Morgensatz /> <p> Heute : <Timeblock /> </p></div>
+	  <div id="title">Hallo <Anrede /> ..... <Morgensatz /> <p><Timeblock /> </p></div>
 	  <div id="score">Wir kommen gleich vorbei</div>
 	  <div id="stats"><p>Heute</p><p>9:00 Frühstück</p><p>12:00 Es ist jetzt Zeit für  Mittagessen</p><p>18:00 Abendessen</p><p>Morgen</p><p>9:00 Frühstück</p><p>12:00 Mittagessen</p><p>15:00 Friseur</p><p>18:00 Abendessen</p></div>
 	  <div id="board"><img src={reactImg} alt="Stylized atom" /></div>
-	  <div id="controls"><p align="left">Hallo</p><p align="right">Hallo hier</p></div>
+	  <div id="controls"><p><dasrot>Hallo</dasrot></p><p><dasgelb>Hallo hier</dasgelb></p></div>
 	</div>
   );
 }
--- src/components/Timeblock.jsx
+++ src/components/Timeblock.jsx
@@ -1,5 +1,9 @@
 import { getZeitpunktNowText } from "../utils";
 export default function Timeblock() {
+function derTag (num) {
+const wochentag = ['Sonntag','Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag' ];
+return wochentag[num];
+}
 var sDate ="test";
   var oDate = new Date();
   if (oDate instanceof Date) {
@@ -9,10 +13,10 @@ var sDate ="test";
     var minutes =      oDate.getMinutes() < 10 ? "0" + oDate.getMinutes() : oDate.getMinutes();
     var hours = oDate.getHours();
     var seconds =      oDate.getSeconds() < 10 ? "0" + oDate.getSeconds() : oDate.getSeconds();
-    sDate =      day +      "." +      month +      "." +      year +      " --- " +      hours +      ":" +       minutes+      ":" +      seconds;
+    var wochentag = derTag(oDate.getDay());
+    sDate =    "Heutiges Datum: "+  day +      "." +      month +      "." +      year +      " Heute ist : " + wochentag +"  ! Es ist jetzt :  " +      hours +      ":" +       minutes +"Uhr";  //+      ":" +      seconds;
     //console.log(userMessage + sDate);
     return sDate;
-  //const theTime = getZeitpunktNowText();
-  //return  {theTime};
+ 
   }
 }
