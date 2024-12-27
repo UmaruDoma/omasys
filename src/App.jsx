4565f84f58198311ba3ee69a83c53d9df2fbfdf7
UmaruDoma <schreiber@screenpaper.de>
2024-12-27 12:18:29 +0000

    modified:   node_modules/.vite/deps/_metadata.json 	modified:   src/App.jsx 	modified:   src/components/DisplayTime.jsx 	modified:   src/grid.css 	modified:   src/index.css

--- src/App.jsx
+++ src/App.jsx
@@ -18,12 +18,12 @@ function App() {
   //       <h1> Heute : <Timeblock /> </h1>
   //     </main>
  //   </div>
-     <div id="grid">
-	  <div id="title">Hallo <Anrede /> ..... <Morgensatz /> <p><Timeblock /> </p></div>
-	  <div id="score">Wir kommen gleich vorbei</div>
-	  <div id="stats"><p>Heute</p><p>9:00 Frühstück</p><p>12:00 Es ist jetzt Zeit für  Mittagessen</p><p>18:00 Abendessen</p><p>Morgen</p><p>9:00 Frühstück</p><p>12:00 Mittagessen</p><p>15:00 Friseur</p><p>18:00 Abendessen</p></div>
-	  <div id="board"><img src={reactImg} alt="Stylized atom" /></div>
-	  <div id="controls"><p><dasrot>Hallo</dasrot></p><p><dasgelb>Hallo hier</dasgelb></p></div>
+    <div id="grid">
+	  <div id="obenlinks"><p>Hallo <Anrede /> ..... <Morgensatz /></p> <p><DisplayTime /> </p></div>
+	  <div id="untenlinks"><p>Wir kommen gleich vorbei</p></div>
+	  <div id="mittelinks"><p>Heute</p><p>9:00 Frühstück <br/> 12:00 Es ist jetzt Zeit für  Mittagessen<br/>18:00 Abendessen<br/>Morgen<br/>9:00 Frühstück<br/>12:00 Mittagessen<br/>15:00 Friseur<br/>18:00 Abendessen</p></div>
+	  <div id="obenmitterechts"><img src={reactImg} alt="Stylized atom" /></div>
+	  <div id="untenrechts"><p><dasrot>Hallo</dasrot></p><p><dasgelb>Hallo hier</dasgelb></p></div>
 	</div>
   );
 }
--- src/components/DisplayTime.jsx
+++ src/components/DisplayTime.jsx
@@ -1,12 +1,13 @@
 import { getZeitpunktNowText } from "../utils";
 export default function DisplayTime() {
-    ddatum = getZeitpunktNowText();
+    const ddatum = getZeitpunktNowText();
     
     return (
     <p>
-    <p>Heutiges Datum:  {ddatum.dday}      .     {ddatum. dmonth}       .     {ddatum. dyear}</p>
-    <p> Heute ist :  {ddatum.dwochtag }</p>
-   <p>  ! Es ist jetzt :        {ddatum.dhours} :      {ddatum.dminutes}  Uhr </p></p>
+     Heutiges Datum: {ddatum.dday}.{ddatum. dmonth}.{ddatum. dyear} <br/>
+     Heute ist :  {ddatum.dwochtag }<br/>
+      ! Es ist jetzt :        {ddatum.dhours} :      {ddatum.dminutes}  Uhr 
+    </p>
     );
  
  
